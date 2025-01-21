6f10c722c027221401ba0759ab25b77ecedd05a2
EC2 Default User <ec2-user@ip-172-31-0-78.ap-northeast-1.compute.internal>
2025-01-21 09:26:39 +0000

    エラー画面表示

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end  
   post 'search', to: 'books#search'
+  get 'search', to: 'homes#error'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :index, :update,]
 end
