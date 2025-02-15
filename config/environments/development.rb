cf39e676c901dc62974ef1e65b08e241d6f14536
EC2 Default User <ec2-user@ip-172-31-12-221.ap-northeast-1.compute.internal>
2025-02-15 08:08:31 +0000

    パスワード変更のための記述→うまく反映されない

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,20 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV["Gmail_Username"] ,
+    password:             ENV["Gmail_Password"] ,
+    authentication:       'plain',
+    enable_starttls_auto:  true
+    }
+    # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
