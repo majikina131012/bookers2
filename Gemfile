ada5b35371d7f6c33101b60ba5abd017dabe0e3a
EC2 Default User <ec2-user@ip-172-31-0-78.ap-northeast-1.compute.internal>
2025-02-17 05:01:40 +0000

    メール送信までできた、メール内のリンクでエラー

--- Gemfile
+++ Gemfile
@@ -55,5 +55,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'net-smtp'
 gem 'devise'
+gem 'dotenv-rails'
 gem 'rails-i18n'
 gem 'devise-i18n'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,20 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                  587,
-    domain:               'gmail.com',
-    user_name:            ENV["Gmail_Username"] ,
-    password:             ENV["Gmail_Password"] ,
-    authentication:       'plain',
-    enable_starttls_auto:  true
-    }
-    # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
@@ -86,6 +73,19 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-   config.active_job.queue_adapter = :inline
+  config.active_job.queue_adapter = :inline
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { protocol: 'https', host: host }
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV["Gmail_Username"] ,
+    password:             ENV["Gmail_Password"] ,
+    authentication:       'plain',
+    enable_starttls_auto:  true
+    }
   config.hosts.clear
 end
