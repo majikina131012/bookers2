a59cf040266852f1c9bb297e55bf19a1a23149cd
EC2 Default User <ec2-user@ip-172-31-12-221.ap-northeast-1.compute.internal>
2025-02-17 14:56:31 +0000

    メール文の日本語化

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-<<<<<<< HEAD
   # Settings specified here will take precedence over those in config/application.rb.
-=======
-    # Settings specified here will take precedence over those in config/application.rb.
->>>>>>> 1f9c132 (パスワード変更機能実装完了)
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
@@ -38,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
@@ -80,17 +76,16 @@ Rails.application.configure do
   config.active_job.queue_adapter = :inline
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
->>>>>>> 1f9c132 (パスワード変更機能実装完了)
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                  587,
-    domain:               'gmail.com',
-    user_name:            ENV["Gmail_Username"] ,
-    password:             ENV["Gmail_Password"] ,
-    authentication:       'plain',
-    enable_starttls_auto:  true
+      address:              'smtp.gmail.com',
+      port:                  587,
+      domain:               'gmail.com',
+      user_name:            ENV["Gmail_Username"] ,
+      password:             ENV["Gmail_Password"] ,
+      authentication:       'plain',
+      enable_starttls_auto:  true
     }
   config.hosts.clear
 end
